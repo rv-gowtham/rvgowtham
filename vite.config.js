c87ea17b5e9fca17db88b8a5179b47e314acbee9
rv-gowtham <rvgowtham6@gmail.com>
2025-07-11 09:35:08 +0530

    new again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === "production" ? "/rvgowtham/" : "/",
+    base: mode === "production" ? "/rvgowtham" : "/",
     server: { port: 5173 },
   };
 });
